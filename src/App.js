9808a97560a8db60d52e3ab8c4da98a7bbcb940e
Deniscartin <100749943+Deniscartin@users.noreply.github.com>
2024-08-25 18:50:21 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-200 h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold text-gray-800">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
